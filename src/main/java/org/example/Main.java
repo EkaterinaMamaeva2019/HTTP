324a23bb38ae56047a9528ed05f6361142d386ab
Ekaterina Mamaeva <katerina.grozdova@mail.ru>
2023-07-28 00:56:34 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,23 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Server server = new Server(64);
         server.start(9999);
+
+        server.addHandler("GET", "/messages", ((request, responseStream) -> {
+            try {
+                server.responseWithoutContent(responseStream,"404", "Not found");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
+        server.addHandler("POST", "/messages", (request, responseStream) ->
+                server.responseWithoutContent(responseStream, "404", "Not found"));
+        server.addHandler("GET", "/", ((request, responseStream) -> server.defaultHandler(responseStream, "spring.png")));
+        server.start(9999);;
     }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -92,5 +93,57 @@ public class Server {
                 IOException e) {
             e.printStackTrace();
         }
+    }
+
+     public void addHandler(String method, String path, Handler handler) {
+         HashMap<String, Handler> handlers = null;
+         if (!handlers.containsKey(method)) {
+            handlers.put(method, handler);
+        }
+        handlers.get(method).put(path, handler);
+    }
+
+    public void responseWithoutContent(BufferedOutputStream out, String responseCode, String responseStatus) throws IOException {
+        out.write((
+                "HTTP/1.1 " + responseCode + " " + responseStatus + "\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    protected void defaultHandler(BufferedOutputStream out, String path) throws IOException {
+        final var filePath = Path.of(".", "public", path);
+        final var mimeType = Files.probeContentType(filePath);
+
+        if (path.equals("/classic.html")) {
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+            return;
+        }
+
+        final var length = Files.size(filePath);
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
     }
 }
